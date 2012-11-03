c1eb74ce438c9a9bf089e6d69361fc7c73d0fdfc
mikechau <mchau@iwu.edu>
2012-11-03 10:16:31 -0500

    added pg gem for heroku deployment

--- db/schema.rb
+++ db/schema.rb
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(:version => 20121103151154) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.string   "token"
-    t.integer  "business_id"
     t.string   "url"
+    t.integer  "business_id"
     t.string   "message"
   end
 
