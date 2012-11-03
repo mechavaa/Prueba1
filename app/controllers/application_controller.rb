9b066c3306c95b1aeea89c12a0cb4f0f82e9b0e9
mikechau <mchau@iwu.edu>
2012-11-03 10:41:42 -0500

    Heroku app, fix facebook api redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,18 @@ class ApplicationController < ActionController::Base
    def signed_in?
     !current_user.nil?
   end
+
+  before_filter :check_url
+
+#redirecting the herokuapp and www version of domain
+	def check_url
+ 	 url = request.url
+  	if url.include?('nfctag.herokuapp.com')
+ 	   redirect_to ('http://nfctag.herokuapp.com')
+ 		elsif url.include?('www.nfctag.herokuapp.com')
+  	  redirect_to ('http://nfctag.herokuapp.com')        
+ 	 	end    
+	end
 end
 
 
