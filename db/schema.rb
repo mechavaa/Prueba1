0067a425f532c15d2673f761bdc2010b96bc3436
Joe Villanueva <spcorbust@hotmail.com>
2012-11-03 10:17:53 -0500

    Preparing for pull from Nick's update.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20121103133732) do
 
   create_table "businesses", :force => true do |t|
