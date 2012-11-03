f8a3c7b21dc44c34a30703c6c9d7b57c82310c24
Jean-Nicholas <nick.hould@gmail.com>
2012-11-03 01:57:33 -0500

    Remove bug

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,8 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, '369679243120241', '248b6335922adec91e28757806d467ca', scope: "email,publish_stream,publish_checkins,user_subscriptions,user_interests"
+
+	provider :facebook, '369679243120241', '248b6335922adec91e28757806d467ca', scope: "email,publish_stream"
 end
 
 
