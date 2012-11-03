0830ba303ba9ed2f00d53bacf7ca89e3cf1a8a87
Jean-Nicholas <nick.hould@gmail.com>
2012-11-03 13:03:24 -0500

    change keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,8 +2,6 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :facebook, '503254463032442', '9af35a855a3504b3d8cd7b205105dd58', :scope => "email,publish_stream"
-
-
 end
 
 
