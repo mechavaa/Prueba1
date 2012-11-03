87f7d3fc56f143ce12db719774f047c5b622c330
Jean-Nicholas <nick.hould@gmail.com>
2012-11-03 11:35:01 -0500

    Add production key and id

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, '369679243120241', '248b6335922adec91e28757806d467ca', :scope => "email,publish_stream"
+	provider :facebook, '431464540247434', '9b149b516c312e6989957786554e54a6', :scope => "email,publish_stream"
 
 
 end
